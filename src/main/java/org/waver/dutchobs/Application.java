11f6c2c36fe9f93dad80b34d4539a7362473809f
Martijn van der Pauw <martijn.vanderpauw@meteogroup.com>
2016-03-18 19:59:41 +0100

    add slf4j and lombok

--- src/main/java/org/waver/dutchobs/Application.java
+++ src/main/java/org/waver/dutchobs/Application.java
@@ -1,17 +1,18 @@
 package org.waver.dutchobs;
+import lombok.extern.slf4j.Slf4j;
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-
-
-
+@Slf4j
 @SpringBootApplication
 @ComponentScan
 public class Application {
 	
 	public static void main(String[] args){
-		System.out.println("starting application");
+		log.info("starting application");
 		SpringApplication.run(Application.class,args);
 	}
 
